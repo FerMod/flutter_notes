af6e7122018b9e2fae00e290c96958d2699069ff
FerMod <tudelaigarcia@gmail.com>
2020-12-10 20:56:52 +0100

    Add function to init emulators

--- lib/globals.dart
+++ lib/globals.dart
@@ -10,4 +10,7 @@ class Global {
 
   /// A constant that is true if the application was compiled in debug mode.
   static final bool isDebugMode = kDebugMode;
+
+  /// A constant that represents if should use the firebase emulator.
+  static final bool useFirebaseEmulator = true;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
-import 'dart:developer' as developer;
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'data/app_options.dart';
-import 'menu/rich_text_editor.dart';
+import 'globals.dart';
 import 'model_binding.dart';
 import 'screens/home_page.dart';
 
-void main() => runApp(NotesApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  _initFirebaseEmulator(Global.useFirebaseEmulator);
+  runApp(NotesApp());
+}
+
+void _initFirebaseEmulator(bool useEmulators) async {
+  if (!useEmulators) return;
+
+  final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+
+  // Switch host based on platform.
+  final firestoreHost = isAndroid ? '10.0.2.2:8080' : 'localhost:8080';
+  FirebaseFirestore.instance.settings = Settings(host: firestoreHost, sslEnabled: false, persistenceEnabled: false);
+
+  // await FirebaseAuth.instance.setPersistence(Persistence.NONE);
+  // final authHost = isAndroid ? '10.0.2.2:9099' : 'localhost:9099';
+  // FirebaseAuth.instance.useEmulator(authHost);
+}
 
 class NotesApp extends StatelessWidget {
   // This widget is the root of the application.
@@ -39,6 +60,9 @@ class NotesApp extends StatelessWidget {
             //   appBar: AppBar(title: Text('Test')),
             //   body: RichTextEditor(onSubmitted: (value) => developer.log(value)),
             // ),
+            // routes: <String, WidgetBuilder>{
+            //   '/notes': (context) => NotesListScreen(),
+            // },
           );
         },
       ),
