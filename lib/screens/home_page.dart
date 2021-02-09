ff873703ccfd97ed447e1daab9367486d22f8d83
FerMod <tudelaigarcia@gmail.com>
2021-02-09 17:32:49 +0100

    Add utility features for testing

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,20 +2,22 @@ import 'dart:developer' as developer;
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_notes/data/models/user_model.dart';
 
-import '../data/app_options.dart';
-import '../data/db/user_entity.dart';
 import '../data/firebase_service.dart';
+import '../data/models/user_model.dart';
 import '../widgets/drawer_menu.dart';
 
 class HomePage extends StatelessWidget {
+  final userData = UserData<UserModel>(collection: 'users');
+  // final _controller = StreamController<UserModel>.broadcast();
+
+  // HomePage() {
+  //   userData.stream((snapshot) => UserModel.fromSnapshot(snapshot)).pipe(_controller);
+  // }
   @override
   Widget build(BuildContext context) {
-    final options = AppOptions.of(context);
     final localizations = AppLocalizations.of(context);
-    final _dropdownMenuItems = _buildDropDownMenuItems(AppLocalizations.supportedLocales);
-
+    // StreamSubscription<UserModel> _streamSubscription;
     return Scaffold(
       appBar: AppBar(
         title: Text(localizations.homepage),
@@ -33,55 +35,83 @@ class HomePage extends StatelessWidget {
       //   ),
       // ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            padding: EdgeInsets.all(20.0),
-            child: DropdownButton(
-              value: options.locale.languageCode,
-              items: _dropdownMenuItems,
-              onChanged: (value) => AppOptions.update(
-                context,
-                options.copyWith(locale: Locale(value)),
-              ),
-            ),
-          ),
-          Container(
+            padding: EdgeInsets.all(8.0),
             child: ElevatedButton(
-              child: const Text('Test'),
+              child: const Text('Sign In Anonymously'),
               onPressed: () {
-                final user = UserData<UserModel>(collection: 'users');
-                user.signInAnonymously();
-                var stream = user.stream((snapshot) => UserModel.fromSnapshot(snapshot));
-                stream.map((i) => 'Stream: $i').listen(print);
+                debugPrint('(Sign In Anonymously)');
+                userData.signInAnonymously();
               },
             ),
           ),
           Container(
+            padding: EdgeInsets.all(8.0),
             child: ElevatedButton(
               child: const Text('Sign In'),
               onPressed: () {
-                final userData = UserData<UserModel>(collection: 'users');
+                debugPrint('(Sign In)');
                 userData.signIn('test@email.com', 'password123');
-                userData.data((snapshot) => UserModel.fromSnapshot(snapshot)).then((value) => developer.log(value.toString()));
               },
             ),
           ),
           Container(
+            padding: EdgeInsets.all(8.0),
             child: ElevatedButton(
               child: const Text('Sign Up'),
-              onPressed: () async {
-                final userData = UserData<UserModel>(collection: 'users');
+              onPressed: () {
+                debugPrint('(Sign Up)');
                 userData.signUp('test@email.com', 'password123');
-                userData.data((snapshot) => UserModel.fromSnapshot(snapshot)).then((value) => developer.log(value.toString()));
               },
             ),
           ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: const Text('Sign Out'),
+              onPressed: () {
+                debugPrint('(Sign Out)');
+                userData.signOut();
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              child: const Text('Current User'),
+              onPressed: () {
+                developer.log('Current user: ${userData.currentUser}');
+              },
+            ),
+          ),
+          // Container(
+          //   padding: EdgeInsets.all(8.0),
+          //   child: Row(
+          //     children: [
+          //       ElevatedButton(
+          //         child: const Text('Subscribe Stream'),
+          //         onPressed: () {
+          //           debugPrint('(subscribe)');
+          //           final stream = _controller.stream;
+          //           _streamSubscription = stream.listen((event) {
+          //             debugPrint('Stream: ${event.toString()}');
+          //           });
+          //         },
+          //       ),
+          //       ElevatedButton(
+          //         child: const Text('Unsubscribe Stream'),
+          //         onPressed: () {
+          //           debugPrint('(unsubscribe)');
+          //           _streamSubscription?.cancel();
+          //         },
+          //       ),
+          //     ],
+          //   ),
+          // ),
         ],
       ),
     );
-  }
-
-  List<DropdownMenuItem<String>> _buildDropDownMenuItems(List<Locale> listItems) {
-    return listItems.map((e) => DropdownMenuItem(value: e.languageCode, child: Text(e.languageCode))).toList();
   }
 }
--- lib/widgets/drawer_menu.dart
+++ lib/widgets/drawer_menu.dart
@@ -1,11 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '../data/firebase_service.dart';
+import '../data/models/user_model.dart';
 import '../screens/home_page.dart';
 import '../screens/notes_list.dart';
+import '../screens/sign_in.dart';
+import '../screens/sign_up.dart';
+import 'drawer_header.dart';
+import 'user_account.dart';
+import 'user_account_dropdown.dart';
 
 class DrawerMenu extends StatelessWidget {
+  DrawerMenu({Key key}) : super(key: key);
+
+  final userData = UserData<UserModel>(collection: 'users');
+
+  Widget _buildHeader() {
+    final user = userData.currentUser;
+
+    Widget headerWidget;
+
+    if (user != null) {
+      Widget currentAccountPicture;
+      if (user?.photoURL != null) {
+        currentAccountPicture = Image.network(user.photoURL);
+      }
+
+      Widget accountName;
+      if (user?.displayName != null) {
+        accountName = Text(user.displayName);
+      }
+
+      Widget accountEmail;
+      if (user?.email != null) {
+        accountEmail = Text(user.email);
+      }
+
+      final otherAccountsPictures = <Widget>[
+        currentAccountPicture,
+        currentAccountPicture,
+      ];
+      // headerWidget = UserAccountsDrawerHeader(
+      //   currentAccountPicture: currentAccountPicture,
+      //   accountName: accountName,
+      //   accountEmail: accountEmail,
+      //   onDetailsPressed: () {},
+      //   //otherAccountsPictures: otherAccountsPictures,
+      // );
+
+      // headerWidget = UserAccountDropdown(
+      //   accountPicture: CircleAvatar(
+      //     child: const Icon(Icons.account_circle),
+      //   ),
+      //   accountName: accountName,
+      //   accountEmail: accountEmail,
+      //   showArrow: true,
+      //   onTap: () {},
+      // );
+
+      // headerWidget = ListTile(
+      //   leading: CircleAvatar(
+      //     child: Image.network(user.email) ,
+      //   ),
+      //   title: accountName,
+      //   subtitle: accountEmail,
+      //   onTap: () {},
+      // );
+      headerWidget = UserAccountListTile(
+        imageUrl: user?.photoURL,
+        nameText: user?.displayName,
+        emailText: user?.email,
+      );
+
+      headerWidget = UserAccountDropdown(
+        accountPicture: CircleAvatar(
+          child: const Icon(Icons.account_circle),
+        ),
+        accountName: Text('Test'),
+        accountEmail: Text('Email'),
+        showArrow: true,
+        onTap: () {},
+      );
+    } else {
+      headerWidget = TitleDrawerHeader(
+        child: ListTile(
+          leading: Icon(Icons.account_circle),
+          title: Text('Sign in'),
+          onTap: () {},
+        ),
+      );
+    }
+
+    return headerWidget;
+  }
+
   Future _navigate(BuildContext context, Widget widget) {
+    return Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) => widget),
+    );
+  }
+
+  Future _navigateReplace(BuildContext context, Widget widget) {
     return Navigator.of(context).pushReplacement(
       MaterialPageRoute(builder: (context) => widget),
     );
@@ -14,49 +110,41 @@ class DrawerMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
-          Container(
-            height: 90,
-            padding: EdgeInsets.zero,
-            margin: EdgeInsets.zero,
-            child: DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-              ),
-              child: Text(
-                localizations.drawerTitle,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: Theme.of(context).textTheme.headline5.fontSize,
-                ),
-              ),
+          TitleDrawerHeader(
+            child: Text(
+              localizations.drawerTitle,
+              style: theme.textTheme.headline5,
             ),
           ),
+          _buildHeader(),
           ListTile(
             leading: Icon(Icons.home),
             title: Text(localizations.homepage),
-            onTap: () => _navigate(context, HomePage()),
+            onTap: () => _navigateReplace(context, HomePage()),
           ),
           Divider(),
           ListTile(
             leading: Icon(Icons.sticky_note_2),
             title: Text(localizations.note(2)),
-            onTap: () => _navigate(context, NotesListScreen()),
+            onTap: () => _navigateReplace(context, NotesListScreen()),
           ),
-          /* 
-         ListTile(
-            leading: Icon(Icons.storage),
-            title: Text(localizations.questions),
-            onTap: () {
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => QuestionList()),
-              );
-            },
+          ListTile(
+            leading: Icon(Icons.login),
+            title: Text(localizations.signIn),
+            onTap: () => _navigate(context, SignInScreen()),
           ),
-          */
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text(localizations.signUp),
+            onTap: () => _navigate(context, SignUpScreen()),
+          ),
         ],
       ),
     );
