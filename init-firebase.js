680f26632fa346c4f74dc314e1343e47fa029ab2
FerMod <FerMod@users.noreply.github.com>
2021-02-13 11:58:56 +0000

    deploy: 48b7218f216e9afbad914f4ba1f928e1ae9a1184

---
+++ init-firebase.js
@@ -0,0 +1,19 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before "init-firebase.js".');
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyDKl1e5UEWKl1j4CrCKENTHhKO8-XWCHfg",
+  authDomain: "notesproject-ab12c.firebaseapp.com",
+  databaseURL: "https://notesproject-ab12c.firebaseio.com",
+  projectId: "notesproject-ab12c",
+  storageBucket: "notesproject-ab12c.appspot.com",
+  messagingSenderId: "787552385461",
+  appId: "1:787552385461:web:900c05d7826287726501fc",
+  // measurementId: "G-PZZ7F85GVT"
+};
+
+// Initialize Firebase
+var app = firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+firebase.firestore();
