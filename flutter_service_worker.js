ce62404e119614d81b5beb8e3ab0969a4d88adc4
FerMod <FerMod@users.noreply.github.com>
2021-02-13 17:46:51 +0000

    deploy: 48151d6daedb346b3ff43f84ee6aba356f130465

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,8 +30,8 @@ const RESOURCES = {
 "version.json": "288bb70cab97a04c98705ad0764d8824",
 "manifest.json": "83474c44f12aba1a121130f0e4dc3ddb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "f220afd00278311b7f071b97635d8ed3",
-"/": "f220afd00278311b7f071b97635d8ed3",
+"index.html": "2887356de64e8a0fe3b691333778579f",
+"/": "2887356de64e8a0fe3b691333778579f",
 "main.dart.js_3.part.js": "f68eebecf4db6dcb41da5eff309e7dcd"
 };
 
