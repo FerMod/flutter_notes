7b6bbbdd9f43c34f7c2e448070c8502cdb1afffd
FerMod <tudelaigarcia@gmail.com>
2021-03-26 15:11:58 +0100

    Extract widgets to different files

--- lib/widgets/drawer_menu.dart
+++ lib/widgets/drawer_menu.dart
@@ -1,15 +1,13 @@
-// ignore: unused_import
-import 'dart:developer' as developer;
-
-import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:package_info_plus/package_info_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_notes/widgets/version_widget.dart';
 
 import '../data/firebase_service.dart';
 import '../data/models/user_model.dart';
 import '../screens/notes_list.dart';
 import '../screens/sign_in.dart';
 import '../screens/sign_up.dart';
+import 'about_app_widget.dart';
 import 'drawer_header.dart';
 import 'user_account.dart';
 
@@ -98,85 +96,9 @@ class DrawerMenu extends StatelessWidget {
           ..._buildDrawerChildren(context),
           const Divider(),
           const AboutAppWidget(),
-          VersionWidget(),
+          const VersionWidget(),
         ],
       ),
-    );
-  }
-}
-
-class VersionWidget extends StatelessWidget {
-  const VersionWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: FutureBuilder<PackageInfo>(
-        future: PackageInfo.fromPlatform(),
-        builder: (context, snapshot) {
-          final data = snapshot.data;
-          final version = data != null ? 'v${data.version}' : '';
-          return Text(
-            version,
-            style: theme.textTheme.caption,
-            textAlign: TextAlign.center,
-          );
-        },
-      ),
-    );
-  }
-}
-
-class AboutAppWidget extends StatelessWidget {
-  const AboutAppWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    final defaultPackageInfo = PackageInfo(
-      appName: 'Unknown',
-      packageName: 'Unknown',
-      version: 'Unknown',
-      buildNumber: 'Unknown',
-    );
-
-    return FutureBuilder<PackageInfo>(
-      future: PackageInfo.fromPlatform(),
-      initialData: defaultPackageInfo,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          developer.log('PackageInfo Snapshot Error: ${snapshot.error}');
-        }
-        developer.log('PackageInfo ConnectionState: ${snapshot.connectionState}');
-
-        final data = snapshot.hasData ? snapshot.data! : defaultPackageInfo;
-        final legalNotice = 'Copyright (C) 2021  Ferran Tudela\n'
-            '\n'
-            'This program is free software: you can redistribute it and/or modify '
-            'it under the terms of the GNU General Public License as published by '
-            'the Free Software Foundation, either version 3 of the License, or '
-            '(at your option) any later version.\n'
-            '\n'
-            'This program is distributed in the hope that it will be useful, '
-            'but WITHOUT ANY WARRANTY; without even the implied warranty of '
-            'MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the '
-            'GNU General Public License for more details.\n'
-            '\n'
-            'You should have received a copy of the GNU General Public License '
-            'along with this program.  If not, see <https://www.gnu.org/licenses/>.';
-
-        return AboutListTile(
-          icon: const Icon(Icons.info_outline),
-          applicationName: data.appName,
-          applicationVersion: '${data.version} (build: ${data.buildNumber})',
-          //applicationIcon: ImageIcon(),
-          applicationLegalese: legalNotice,
-          child: Text(localizations.aboutApp),
-        );
-      },
     );
   }
 }
