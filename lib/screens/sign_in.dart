42a0282ecc6224dbdf92f332a4ef110f5ba91832
FerMod <tudelaigarcia@gmail.com>
2021-06-09 20:51:51 +0200

    Fix imports

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import '../data/models.dart';
 import '../routes.dart';
